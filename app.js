622e50e32f08381b681d39cc0db926ad0bc54ded
robert-Dev-Full <robinhoc@hotmail.com>
2022-10-24 20:35:33 -0300

    teste

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('teste')
+const http = require('http')
+const port = 3000;
+
+const server = http.createServer((req,res) => {
+    res.statusCode = 200
+    res.setHeader('Contant-Type', 'text/plain')
+
+    res.end('teste')
+})
+
+server.listen(port, () => {
+    console.log('Servidor iniciado 2')
+})

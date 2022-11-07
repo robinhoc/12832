b3d77cebdc6c3713c2b20cc1d8cb1e90869ce22b
robert-Dev-Full <robinhoc@hotmail.com>
2022-11-07 20:30:51 -0300

    teste

--- app.js
+++ app.js
@@ -1,51 +1,49 @@
-const express = require('express')
+const express = require('express') // cria o servidor web para receber requisições
+const swaggerUi = require('swagger-ui-express') // para documentação visual
+const mongoose = require('mongoose') //conexao ao banco de dados
+
+var routeProduto = require('./routes/produto') // rota onde esta toda info dos produtos
+var routeUsuario = require('./routes/usuario')// rota onde esta toda info dos usuarios
+var middlewares = require('./middlewares/middlewares')
+
 const app = express()
 
-app.use(express.static('public')); // habilitar arquivos da pasta para funcionar pelo servidor
-app.use(express.json()) // pegar o valor do body
+//inicio dados para conexao ao banco
+var url = 'mongodb+srv://app:123@cluster0.i2yjs8v.mongodb.net/?retryWrites=true&w=majority'
+//var url = 'mongodb+srv://app:123@cluster0.tubrkee.mongodb.net/?retryWrites=true&w=majority'
 
-app.get('/', (req, res) => {
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+};
+
+mongoose.connect(url, options);
+
+mongoose.connection.on('connected', () => {
+    console.log('Conectado ao mongodb!!!')
+})
+//fim dados para conexao ao banco
+const swaggerDocument = require('./swagger_output.json'); // local onde vai saltar a documentação
+
+app.use(express.static('public')) // libera essa pasta para acesso a arquivos
+app.use(express.json()); //para converter a recepcao do Json
+
+app.use(middlewares.request);
+
+app.use('/documentacao', swaggerUi.serve, swaggerUi.setup(swaggerDocument)) // gera a documentação
+
+//app.use('/produtos', middlewares.autenticacao, routeProduto); // adiciona a rota dos produtos http://localhost:3000/produtos
+app.use('/produtos', routeProduto); // adiciona a rota dos produtos http://localhost:3000/produtos
+app.use('/usuarios', routeUsuario);// adiciona a rota dos usuarios http://localhost:3000/usuarios
+
+app.get('/', (req, res) => { // cai na raiz do servidor
+    // #swagger.tags = ['Root']   
     res.send('Opa')
 })
 
-app.get('/produtos', (req, res) => {
-    console.log(req.query.nome +'-'+req.query.sobrenome)
-    res.send('Listar produtos')
-})
+app.listen(process.env.PORT || 3000);
 
-app.get('/produtos/:codigo', (req, res) => {
-    var codFiltro = req.params.codigo;
-
-    res.send('Buscar produtos '+codFiltro)
-})
-
-app.post('/produtos', (req, res) => {
-    
-    let produto = {
-        codigo: req.body.codigo,
-        descricao: req.body.descricao
-    }
-    console.log(produto)
-
-    res.status(201).send('Criar categoria')
-})
-
-app.put('/produtos', (req, res) => {
-    let produto = {
-        codigo: req.body.codigo,
-        descricao: req.body.descricao
-    }    
-    console.log(produto)
-    res.send('Alterar categoria')
-})
-
-app.delete('/produtos/:codigo', (req, res) => {
-    var codDelete = req.params.codigo;
-
-    res.send('Excluir produto '+codDelete)    
-})
-
-
-app.listen(3000, () => {
-    console.log('Servidor web ok!')
-})
+//app.listen(3000, () => { // inciia o servidor web
+//    console.log('Servidor web ok!')
+//})
